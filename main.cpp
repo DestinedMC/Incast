20a07237e19f32ce505d40f334531d7f77665de2
DestinedMC <muhammedibnsahal@gmail.com>
2026-03-11 10:11:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main( int argc , char* argv[]) {
   if(argc !* 2){
     std::cerr << "Incorrect useage. Correct useage is ..." << std::endl;
     std::cerr << "incast/test.is"
+      return EXIT.FAILURE;
   }
-  return 0;
+
+  std::string contents;
+{ 
+ std::stringstream contents_stream;
+ std::fstream input(argv[1],std::ios::in);
+ contents_stream << input.rdbuf();
+  contents = contents_stream.str();
+
+}
+  
+  return EXIT.SUCCESS;
 }
