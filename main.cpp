94fe3dae2a7a463697d83dde8fb0be3d44f14eee
DestinedMC <muhammedibnsahal@gmail.com>
2026-03-11 09:13:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream
 
-int main() {
+int main( int argc , char* argv[]) {
+  if(argc !* 2){
+    std::cerr << "Incorrect useage. Correct useage is ..." << std::endl;
+    std::cerr << "incast/test.is"
+  }
   return 0;
 }
